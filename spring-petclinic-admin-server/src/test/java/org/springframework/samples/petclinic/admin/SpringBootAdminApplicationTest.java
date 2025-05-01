317b8a02a55b158ee7f746d953c581bb61adea55
JonasAugust12 <cas.nmtoan@gmail.com>
2025-05-01 17:14:43 +0700

    feat: add test for all service

---
+++ spring-petclinic-admin-server/src/test/java/org/springframework/samples/petclinic/admin/SpringBootAdminApplicationTest.java
@@ -0,0 +1,12 @@
+package org.springframework.samples.petclinic.admin;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootAdminApplicationTest {
+
+    @Test
+    void contextLoads() {
+    }
+}
--- spring-petclinic-api-gateway/src/test/java/org/springframework/samples/petclinic/api/application/VisitsServiceClientIntegrationTest.java
+++ spring-petclinic-api-gateway/src/test/java/org/springframework/samples/petclinic/api/application/VisitsServiceClientIntegrationTest.java
@@ -62,4 +62,3 @@ class VisitsServiceClientIntegrationTest {
     }
 
 }
-// change
